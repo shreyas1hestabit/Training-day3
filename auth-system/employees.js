05a5c4521e13a869e235cc867c0727b454b5e880
Shreya Singhal <shreyas1.hestabit@gmail.com>
2026-01-08 16:21:51 +0530

    Commit 3: add employee records with roles

---
+++ auth-system/employees.js
@@ -0,0 +1,10 @@
+const ROLES = require("./roles");
+
+const employees = [
+  { name: "Aman", role: ROLES.ADMIN },
+  { name: "Neha", role: ROLES.MANAGER },
+  { name: "Ravi", role: ROLES.EMPLOYEE },
+  { name: "Pooja", role: ROLES.INTERN }
+];
+
+module.exports = employees;
